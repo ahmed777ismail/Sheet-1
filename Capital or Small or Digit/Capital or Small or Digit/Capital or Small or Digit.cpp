5717bf3c10683c0848c968513b5fc2c135d04e85
ahmed ismail <87954455+ahmed777ismail@users.noreply.github.com>
2024-05-09 12:13:05 +0300

    Add files via upload
    Capital or Small

---
+++ Capital or Small or Digit/Capital or Small or Digit/Capital or Small or Digit.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    char myChar;
+    cin >> myChar;
+	if (myChar >=97 && myChar<=122)
+	{
+		cout << "ALPHA" << endl;
+		cout << "IS SMALL" << endl;
+	}
+	else if (myChar >= 65 && myChar <= 90) {
+		cout << "ALPHA" << endl;
+		cout << "IS CAPITAL" << endl;
+	}
+	else
+	{
+		cout << "IS DIGIT";
+	}
+}
