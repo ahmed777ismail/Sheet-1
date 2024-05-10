5b73a7b42c985a090d448662be1812a772f00d2e
ahmed ismail <87954455+ahmed777ismail@users.noreply.github.com>
2024-05-10 12:34:13 +0300

    Add files via upload
    Char

---
+++ Char/Char/Char.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	char x;
+	cin >> x;
+	if (x >= 'A' && x <= 'Z')
+	{
+		cout << (char)(x + 32);
+	}
+	else
+	{
+		cout << (char)(x - 32);
+	}
+}
+
